d4584b95a2efd3d7ad65c96bb9ec0c30c9b15167
guldisfp <guldisfp@users.noreply.github.com>
2019-01-06 12:14:08 +0100

    Started implementation of wifi-communication through the esp8266 chip

---
+++ Inc/esp8266_interface.h
@@ -0,0 +1,13 @@
+/*
+ * esp8266_interface.h
+ *
+ *  Created on: 6 jan. 2019
+ *      Author: fp
+ */
+
+#ifndef ESP8266_INTERFACE_H_
+#define ESP8266_INTERFACE_H_
+
+
+
+#endif /* ESP8266_INTERFACE_H_ */
---
+++ Src/esp8266_interface.c
@@ -0,0 +1,8 @@
+/*
+ * esp8266_interface.c
+ *
+ *  Created on: 6 jan. 2019
+ *      Author: fp
+ */
+
+
